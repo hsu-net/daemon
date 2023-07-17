3dd73c481576fbf848f3e69e4471b64830b2984e
hsu <seayxu@gmail.com>
2023-07-17 19:18:00 +0800

    Updated Github Actions

--- nuke/Build.Github.cs
+++ nuke/Build.Github.cs
@@ -33,6 +33,13 @@ using static Nuke.Common.Tools.GitHub.GitHubTasks;
     OnPushBranches = new[] { MainBranch, PreviewBranch },
     InvokedTargets = new[] { nameof(Deploy) },
     ImportSecrets = new[] { nameof(NuGetApiKey), nameof(MyGetApiKey) },
+    ReadPermissions = new GitHubActionsPermissions[]
+    {
+        GitHubActionsPermissions.IdToken,
+        GitHubActionsPermissions.Packages,
+        GitHubActionsPermissions.Pages,
+        GitHubActionsPermissions.Actions
+    },
     WritePermissions = new GitHubActionsPermissions[]
     {
         GitHubActionsPermissions.IdToken,
